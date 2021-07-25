83e4432b58dbc7ebc44edcd8620def7f7e617ea2
AndresLoaPuris <49095669+AndresLoaPuris@users.noreply.github.com>
2021-07-25 00:57:31 -0500

    build

---
+++ build/precache-manifest.8e47ab6b0dd7d9b0a5608588a78b6630.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab4a8a869e6e1e4918852b8d12791a95",
+    "url": "/newsio/index.html"
+  },
+  {
+    "revision": "61b4e300b3c213fa7b82",
+    "url": "/newsio/static/css/3.23279d8d.chunk.css"
+  },
+  {
+    "revision": "0c9ea2d6f0e21ca64cce",
+    "url": "/newsio/static/css/main.3e0f35e7.chunk.css"
+  },
+  {
+    "revision": "fda45d74645791d0f697",
+    "url": "/newsio/static/js/0.4c6cee06.chunk.js"
+  },
+  {
+    "revision": "e2a608f3b392ed4f1f89ddb34b2dbcda",
+    "url": "/newsio/static/js/0.4c6cee06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61b4e300b3c213fa7b82",
+    "url": "/newsio/static/js/3.b9935d35.chunk.js"
+  },
+  {
+    "revision": "774152ec3dfb5dadc6880f2e7f79e400",
+    "url": "/newsio/static/js/3.b9935d35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93b8c6df32dbf98bedf0",
+    "url": "/newsio/static/js/4.957e9596.chunk.js"
+  },
+  {
+    "revision": "e3906660226bbe07dd30",
+    "url": "/newsio/static/js/5.86160110.chunk.js"
+  },
+  {
+    "revision": "0c9ea2d6f0e21ca64cce",
+    "url": "/newsio/static/js/main.b692b780.chunk.js"
+  },
+  {
+    "revision": "afa4a4eacd63a1ed0629",
+    "url": "/newsio/static/js/runtime-main.0121cee7.js"
+  }
+]);
